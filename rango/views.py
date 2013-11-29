ff56622b6faa1b0f28cb5739e101a9aff93666ee
Courage Mabrey <cmabrey@arrowtesting.com>
2013-11-29 17:22:52 +0000

    initial commit

---
+++ rango/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+	context = RequestContext(request)
+	context_dict = {'boldmessage': 'am from the context'}
+	return render_to_response('rango/index.html', context_dict, context)
+
+
+
+def about(request):
+	context = RequestContext(request)
+	context_dict = {'welcome':'page serverd from about view'}
+	return render_to_response('rango/about.html', context_dict, context)
